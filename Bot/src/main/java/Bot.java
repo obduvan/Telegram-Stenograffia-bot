2f80dfe91c913dcd25f3c162a2a0f0c92a895c85
obduvan <obduvann@gmail.com>
2020-10-16 14:03:53 +0500

    fix losted files

---
+++ Bot/src/main/java/Bot.java
@@ -0,0 +1,71 @@
+import org.apache.commons.io.IOUtils;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Bot extends TelegramLongPollingBot {
+
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                case "/start":
+                    String defaultMsg = null;
+                    try {
+                        defaultMsg = getDefaultMessage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    sendMsg(message, defaultMsg);
+                    break;
+                case "/authors":
+                    sendMsg(message, System.getenv("AUTHORS"));
+                    break;
+                case "/works":
+                    sendMsg(message, "ща");
+                    break;
+                default:
+            }
+        }
+
+    }
+
+    public String getDefaultMessage() throws IOException {
+        String content;
+        content = new String(Files.readAllBytes(Paths.get("src\\main\\resources\\default message\\defaultMessage.md")));
+        return content;
+    }
+
+    public String getBotUsername() {
+        return System.getenv("NAMEBOT");
+    }
+
+    public String getBotToken() {
+        return System.getenv("TOKEN");
+    }
+}
