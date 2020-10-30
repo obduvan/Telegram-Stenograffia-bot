ad3742ae7813bc89056cec390234c2e3d265b8aa
obduvan <obduvann@gmail.com>
2020-10-31 03:02:34 +0500

    fix

--- Bot/src/main/java/Bot.java
+++ Bot/src/main/java/Bot.java
@@ -1,5 +1,5 @@
-import org.apache.commons.io.IOUtils;
-import org.checkerframework.checker.units.qual.C;
+import constants.ConstantPath;
+import constants.Constants;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -7,31 +7,84 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.*;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class Bot extends TelegramLongPollingBot {
+    private ControlState controlState;
+    private List<Map<String, String>> dataList;
+    private Works works;
+    private StandardFunctions standardFunctions;
 
-    private SendMessage setMessage(Message message) {
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.enableMarkdown(true);
-        sendMessage.setChatId(message.getChatId().toString());
-        return sendMessage;
+    public Bot(List<Map<String, String>> idataList) {
+        dataList = idataList;
+        controlState = new ControlState();
+        works = new Works();
+        standardFunctions = new StandardFunctions();
     }
 
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            try {
+                var userId = message.getFrom().getId();
+                handleInputMessage(message, userId);
+                action(userId);
+            } catch (IOException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
 
-    private void sendMsg(Message message, String text) {
-        SendMessage sendMessage = setMessage(message);
-        sendMessage.setText(text);
+    private ConcurrentHashMap<String, BotState> getBotStateMap() throws IOException {
+        ConcurrentHashMap<String, BotState> botStateMap = new ConcurrentHashMap<>() {};
+        botStateMap.put(Constants.START, BotState.ASK_HELP);
+        var botStates = BotState.values();
+        Scanner s = new Scanner(new File(ConstantPath.commands));
+        int k = 0;
+        while (s.hasNext()) {
+            botStateMap.put(s.next(), botStates[k]);
+            k++;
+        }
+        s.close();
+        return botStateMap;
+    }
 
+    private void handleInputMessage(Message message, Integer userId) throws IOException {
+        String inputMsg = message.getText();
+        var botStateMap = getBotStateMap();
+        var botState = botStateMap.getOrDefault(inputMsg, BotState.NONE);
 
+        controlState.updateStatesMap(userId, botState, message);
+
+    }
+
+    private void action(Integer userId) throws IOException {
+        SendMessage sendMessage;
+        var state = controlState.getStateUser(userId);
+        switch (state.getStatus()) {
+            case ASK_HELP:
+                sendMessage = standardFunctions.sendHelpMsg(state.getLastMessage());
+                sendMsg(sendMessage);
+                break;
+            case ASK_AUTHORS:
+                sendMessage = standardFunctions.sendAuthorsMsg(state.getLastMessage());
+                sendMsg(sendMessage);
+                break;
+            case ASK_WORKS:
+                List<SendPhoto> sendPhotoList = works.sendWorksMsg(state, dataList);
+                sendMsg(sendPhotoList);
+                if (checkState(state))
+                    sendMsg(standardFunctions.sendEndedWorks(state.getLastMessage()));
+                break;
+        }
+    }
+
+    public void sendMsg(SendMessage sendMessage) {
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
@@ -39,78 +92,33 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-
-    private String[] parsePhotos(String photos) {
-        return photos.split(",");
-    }
-
-
-    public void sendMsgWorks(Message message) throws SQLException {
-        ConnectDataBase dataBase = new ConnectDataBase();
-        ResultSet rs = dataBase.state.executeQuery("select * from ARTS;");
-
-        SendPhoto sendPhotoRequest = new SendPhoto();
-        sendPhotoRequest.setChatId(message.getChatId().toString());
-
-        for (int i = 0; i < 8; i++) {
-            rs.next();
-            String title = rs.getString("TITLE");
-            String[] photos = parsePhotos(rs.getString("PHOTOS"));
-            String coordinates = String.format("\n%s", rs.getString("COORDINATES"));
-
-
-            sendPhotoRequest.setCaption(String.format("%s %s", title, coordinates));
-            sendPhotoRequest.setPhoto(photos[0].strip());
-
-            try {
-                execute(sendPhotoRequest);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+    public void sendMsg(List<SendPhoto> sendPhotoList) {
+        try {
+            for (SendPhoto sendPhoto : sendPhotoList)
+                execute(sendPhoto);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
-
-        dataBase.close();
     }
 
-
-    public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        if (message != null && message.hasText()) {
-            switch (message.getText()) {
-                case "/help":
-                case "/start":
-                    try {
-                        String defaultMsg = getDefaultMessage();
-                        sendMsg(message, defaultMsg);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case "/authors":
-                    sendMsg(message, System.getenv("AUTHORS"));
-                    break;
-                case "/works":
-                    try {
-                        sendMsgWorks(message);
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                default:
-            }
-        }
-
+    private boolean checkState(State state) {
+        if (state.getStatus() == BotState.ASK_WORKS)
+            return state.getNumPhotoWorks() >= Constants.NUMWORKS;
+        return false;
     }
 
-    public String getDefaultMessage() throws IOException {
-        return new String(Files.readAllBytes(Paths.get("src\\main\\resources\\default message\\defaultMessage.md")));
-    }
-
-    public String getBotUsername() {
-        return System.getenv("NAMEBOT");
-    }
-
+    @Override
     public String getBotToken() {
-        return System.getenv("TOKEN");
+        return System.getenv(Constants.SYSTOKEN);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return System.getenv(Constants.SYSNAMEBOT);
     }
 }
+
+
+
+
+
--- Bot/src/main/java/Works.java
+++ Bot/src/main/java/Works.java
@@ -1,42 +1,40 @@
 import constants.Constants;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Message;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Works {
-    private Bot bot;
 
-    public Works(Bot ibot) {
-        bot = ibot;
-    }
-
-    public void sendMsg(Message message, Map<String, String> dataLine, State state){
-        SendPhoto sendPhotoRequest = new SendPhoto();
-        sendPhotoRequest.setChatId(message.getChatId().toString());
+    public SendPhoto sendMsg(Message message, Map<String, String> dataLine, State state){
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(message.getChatId().toString());
 
         String numPhoto = state.getNumPhotoWorks().toString();
         String title = dataLine.get(Constants.TITLE);
         String link = dataLine.get(Constants.IDS);
         String photo = dataLine.get(Constants.PHOTOS);
 
-        sendPhotoRequest.setCaption(String.format("%s   %s/%s\n%s", title, numPhoto, Constants.NUMWORKS, link));
-        sendPhotoRequest.setPhoto(photo);
+        sendPhoto.setCaption(String.format("%s   %s/%s\n%s", title, numPhoto, Constants.NUMWORKS, link));
+        sendPhoto.setPhoto(photo);
 
-        bot.sendMsg(sendPhotoRequest);
+        return sendPhoto;
     }
 
 
-    public void sendWorksMsg(State state) {
+    public List<SendPhoto> sendWorksMsg(State state, List<Map<String, String>> dataList) {
+        List<SendPhoto> sendPhotoList = new ArrayList<>();
         int buffer = Constants.BUFFER;
-        var works = new Works(bot);
-        var dataList = bot.getDataList();
         if (state.getNumPhotoWorks() < Constants.NUMWORKS){
             while (buffer != 0) {
-                works.sendMsg(state.getLastMessage(), dataList.get(state.getNumPhotoWorks() - 1), state);
+                sendPhotoList.add(sendMsg(state.getLastMessage(), dataList.get(state.getNumPhotoWorks() - 1), state));
                 state.updateNumPhotoWorks(1);
                 buffer -=1;
             }
         }
+        return sendPhotoList;
     }
 }
 
