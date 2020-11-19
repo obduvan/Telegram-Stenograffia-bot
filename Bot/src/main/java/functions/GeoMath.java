b20aee683c85a27c623e9bf3af52a5249c0b1be1
bypolinafox <polina20012012@yandex.ru>
2020-11-19 18:27:09 +0500

    Added test class for GeoMath

--- Bot/src/main/java/functions/GeoMath.java
+++ Bot/src/main/java/functions/GeoMath.java
@@ -5,6 +5,8 @@ import constants.Constants;
 public class GeoMath {
     public Double getGeoPointsDistance(Double lat1, Double lat2, Double long1, Double long2) {
 
+        if (lat1 > 90 || lat1 < -90 || lat2 > 90 || lat2 < -90 || long1 > 180 || long1 < -180 || long2 > 180 || long2 < -180) return -1.0;
+
         double currLocationLatitudeRads = Math.toRadians(lat2);
         double currLocationLongtitudeRads = Math.toRadians(long2);
 
